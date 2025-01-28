562c6450187b0f6d03e22b03276a92e971674721
otabek-a <otabekabdurasulov245@gmail.com>
2025-01-28 18:07:30 +0500

    student4

--- main.py
+++ main.py
@@ -45,7 +45,49 @@ student2={
         "zip_code": "P571"
     }
 }
-
-
+student3={
+    "id": 1,
+    "name": "sardor",
+    "age": 18,
+    "gender": "male",
+    "contact": "99-192-13-13",
+    "grade_level": "Graded school",
+    "subjects": {
+        "math": 79,
+        "science": 90,
+        "english": 87
+    },
+    "attendance": 92.5,
+    "activities": ["dance club", "HISTORY Club"],
+    "address": {
+        "street": "56 zargaron",
+        "city": "moontepa",
+        "state": "left",
+        "zip_code": "9090"
+    }
+}
+student4 = {
+    "id": 4,
+    "name": "Abdulla",
+    "age": 18,
+    "gender": "male",
+    "contact": "99-111-11-11",
+    "grade_level": "Grade 11",
+    "subjects": {
+        "math": 82,
+        "science": 88,
+        "english": 79
+    },
+    "attendance": 91.0,
+    "activities": ["Chess Club", "HISTORY Club"],
+    "address": {
+        "street": "23 Bog'bon",
+        "city": "Toshkent",
+        "state": "Yashil",
+        "zip_code": "700100"
+    }
+}
 students_table = db.table('students')
 students_table.insert_multiple([student1,student2])
+students_table.insert(student3)
+students_table.insert(student4)
